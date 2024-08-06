719eac14653ac14b5c1ceb2585f7cc5df5a400e1
emreyalcin <yalcnnemre@gmail.com>
2024-08-06 15:43:24 +0300

    validasyon mesajı düzeltildi

--- KuyumcuWebApi/Validators/RegisterValidator.cs
+++ KuyumcuWebApi/Validators/RegisterValidator.cs
@@ -14,6 +14,6 @@ public class RegisterValidator : AbstractValidator<RegisterDto>
         RuleFor(item => item.Email).NotNull().NotEmpty().EmailAddress();
         RuleFor(item => item.Password).NotNull().NotEmpty().Length(3, 50);
         RuleFor(item => item.PasswordConfirm).NotNull().NotEmpty().Equal(el => el.Password).WithMessage("Parolalar eşleşmiyor");
-        RuleFor(item => item.RoleId).NotNull().NotEmpty().Must(item => item == 1 || item == 2).WithMessage("RoleId 0 veya 1 olabilir");
+        RuleFor(item => item.RoleId).NotNull().NotEmpty().Must(item => item == 1 || item == 2).WithMessage("RoleId 1 veya 2 olabilir");
     }
 }
